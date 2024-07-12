dcc698960600e80c7cbfc3fe29926ddc572ec44b
Shubhajit <shubjitgithub@gmail.com>
2024-07-12 17:40:18 +0530

    Done

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -9,7 +9,14 @@ public class IsThereSum {
      * @param target a hypothetical sum of two numbers.
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
-    public boolean check(int[] arr, int target){
+    public boolean check(int[] arr, int target) {
+        for (int i = 0; i < arr.length-1; i++) {
+                if (arr[i] + arr[i+1] == target)
+                    return true;
+           
+
+        }
         return false;
+
     }
 }
