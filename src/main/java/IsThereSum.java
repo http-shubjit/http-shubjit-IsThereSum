2fb27ae4782501d6ee7c0ec0b640c28be84b54ef
Shubhajit <shubjitgithub@gmail.com>
2024-07-12 17:42:35 +0530

    Done

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -13,10 +13,7 @@ public class IsThereSum {
         for (int i = 0; i < arr.length-1; i++) {
                 if (arr[i] + arr[i+1] == target)
                     return true;
-           
-
         }
         return false;
-
     }
 }
